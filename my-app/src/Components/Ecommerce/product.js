1d770e0d2f8b080789a2b5c630998aa84d1b8c23
NikishaPatil <nipatil@redhat.com>
2022-06-29 08:28:52 +0530

    Added-Files

---
+++ my-app/src/Components/Ecommerce/product.js
@@ -0,0 +1,104 @@
+import img1 from "../../Assets/1.svg";
+import img2 from "../../Assets/2.svg";
+import img3 from "../../Assets/3.svg";
+import img4 from "../../Assets/4.svg";
+import img5 from "../../Assets/5.svg";
+import img6 from "../../Assets/6.svg";
+import img7 from "../../Assets/7.svg";
+import img8 from "../../Assets/8.svg";
+import img9 from "../../Assets/9.svg";
+import img10 from "../../Assets/10.svg";
+
+
+
+
+
+export const Cart=[
+    {
+        id: 1,
+        img: img1,
+        itemname: "Spring Red Dress",
+        descrp: "A beautiful dress for you best evenings and important dates",
+        like: 147,
+        amount: "$44.00",},
+
+
+        {id: 2,
+        img: img2,
+        itemname: "Cool Shirt",
+        descrp: "A shirt that will make you look nice and feel relax",
+        like: 42,
+        amount: "$29.00",
+        },
+
+
+        {id: 3,
+        img: img3,
+        itemname: "Military Short",
+        descrp: "An awesome shorts for chillout and trips, blends in with anything",
+        like: 56,
+        amount: "$39.00",},
+
+
+        {id: 4,
+        img: img4,
+        itemname: "Cool Backpack",
+        descrp: "Feeling adventurous? You got to try this awesome backpack",
+        like: 212,
+        amount: "$125.00",},
+
+        {id: 5,
+        img: img5,
+        itemname: "The Bob",
+        descrp: "Everyone needs a nice bob to protect himself from the sun",
+        like: 18,
+        amount: "$15.00",},
+];
+
+export const CartItem =[
+
+        {id: 6,
+        img: img6,
+        itemname: "The Girly Top",
+        descrp: "Wanna look nice and fresh? Why not try this beautiful top",
+        like: 92,
+        amount: "$34.00",},
+
+
+        {id: 7,
+        img: img7,
+        itemname: "The T-Shirt",
+        descrp: "A nice t-shirt that goes everywhere with anything, try it",
+        like: 269,
+        amount: "$18.00",},
+
+
+        {id: 8,
+        img: img8,
+        itemname: "The Gala Dress",
+        descrp: "A beautiful dress for your business evenings and meetings",
+        like: 66,
+        amount: "$89.00",},
+
+
+
+        {id: 9,
+        img: img9,
+        itemname: "The Totte Bag",
+        descrp: "Classy, elegant and simple, this bag has anything you need",
+        like: 423,
+        amount: "$112.00",},
+
+
+
+        {id: 10,
+        img: img10,
+        itemname: "Jeans Skirt",
+        descrp: "A beautiful skirt made with jeans for urban outfits",
+        like: 48,
+        amount: "$55.00",},
+
+
+        
+    
+];
---
+++ my-app/src/Components/Rightbar/Rightbar.js
@@ -0,0 +1,14 @@
+import "./rightbar.css";
+import img2 from "../../Assets/imgg.png";
+
+
+export default function Rightbar() {
+    return(
+        <div className="rightbar-container">
+        <div>
+            <img src={img2} alt="logo" className="bdaylogo"></img>
+        </div>
+    </div>
+);
+    
+}
